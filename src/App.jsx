a5c8d5f5a1f6ea4633da480987873014765c0955
Dev u <uaithegreat001@gmail.com>
2025-09-08 13:55:01 +0100

    teesting some features

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import AppRoutes from "./routes/AppRoutes";
+import AdminDashboard from "./pages/admin/AdminDashboard";
 function App() {
 
   return (
     <>
     <AppRoutes/>
-    
+    <AdminDashboard/>
       
     </>
   )
